5b0199313ca7680bacfb432d1bbbd28e779fcab0
Andri Sul <andri@ordent.co>
2023-06-02 11:24:56 +0700

    remove unused function

--- l005_structs_methods_interfaces/perimeter.go
+++ l005_structs_methods_interfaces/perimeter.go
@@ -35,7 +35,3 @@ func (r Triangle) Area() float64 {
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
-
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
-}
