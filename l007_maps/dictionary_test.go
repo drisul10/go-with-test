8862f5cd361b15ac0108d8f681a9250083e91dab
Andri Sul <andri@ordent.co>
2023-06-02 16:38:32 +0700

    implement update definition

--- l007_maps/dictionary_test.go
+++ l007_maps/dictionary_test.go
@@ -41,7 +41,17 @@ func TestAdd(t *testing.T) {
 		assertError(t, err, ErrWordExists)
 		assertDefinition(t, dictionary, word, definition)
 	})
+}
 
+func TestUpdate(t *testing.T) {
+	word := "test"
+	definition := "this is just a test"
+	dictionary := Dictionary{word: definition}
+	newDefinition := "new definition"
+
+	dictionary.Update(word, newDefinition)
+
+	assertDefinition(t, dictionary, word, newDefinition)
 }
 
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
