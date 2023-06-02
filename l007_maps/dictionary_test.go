152b7d92a7b66565751e00d03c698b75ebbe3f71
Andri Sul <andri@ordent.co>
2023-06-02 16:10:26 +0700

    add case unknown word, and refactor assert error

--- l007_maps/dictionary_test.go
+++ l007_maps/dictionary_test.go
@@ -5,13 +5,31 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
 
-	assertStrings(t, got, want)
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := ErrNotFound
+
+		assertError(t, err, want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
 	if got != want {
