470430aeab95e775511ba69ae5fc00085bc671f5
Andri Sul <andri@ordent.co>
2023-06-02 11:03:41 +0700

    make type circle and make interface shap
    interface shape used to decouple test helper function

--- l005_structs_methods_interfaces/perimater.go
+++ l005_structs_methods_interfaces/perimater.go
@@ -1,8 +1,26 @@
 package structs_methods_interfaces
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 func Perimeter(rectangle Rectangle) float64 {
