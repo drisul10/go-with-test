3e03cbac718e225f169599099ae8dcf63f13d3a0
Andri Sul <andri@ordent.co>
2023-06-02 16:49:34 +0700

    implement delete word

--- l007_maps/dictionary_test.go
+++ l007_maps/dictionary_test.go
@@ -67,6 +67,18 @@ func TestUpdate(t *testing.T) {
 	})
 }
 
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("expected %q to be deleted", word)
+	}
+}
+
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
