d9bce5eb9424fa4ac3dacc1ec62f96b35febacc0
Andri Sul <andri@ordent.co>
2023-06-02 16:24:18 +0700

    refactoring assert definition

--- l007_maps/dictionary_test.go
+++ l007_maps/dictionary_test.go
@@ -23,16 +23,24 @@ func TestSearch(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	word := "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
 
-	got, err := dictionary.Search("test")
-	want := "this is just a test"
+	assertDefinition(t, dictionary, word, definition)
+}
 
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
 
-	assertStrings(t, got, want)
+	if definition != got {
+		t.Errorf("got %q, want %q", got, definition)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
