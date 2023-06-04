57af57825836c38d792bef1d02fd03a79f20e866
Andri Sul <andri@ordent.co>
2023-06-04 16:34:33 +0700

    use channel to tackle race condition!

--- l010_concurrency/check_website.go
+++ l010_concurrency/check_website.go
@@ -1,19 +1,26 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type ResultWebsiteChecker struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultsChan := make(chan ResultWebsiteChecker)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultsChan <- ResultWebsiteChecker{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		result := <-resultsChan
+		results[result.string] = result.bool
+	}
 
 	return results
 }
